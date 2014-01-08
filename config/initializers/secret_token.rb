236871eda2e19b379e42f4f0bedbe239201d6634
Raul Jordan <rauljordan@college.harvard.edu>
2014-01-08 16:25:27 -0600

    Initial biotech rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biotech::Application.config.secret_key_base = '67d47bcf07ceb802290a9818a819fbed7c8f09dc74e2be07b81cb45ec015ab890332c9de78041437d0ddfc4a8cb556eaed9d4175454d3eab7063de3e26347815'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Biotech::Application.config.session_store :cookie_store, key: '_biotech_session'
